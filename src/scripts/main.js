ca7210a95ad720397f2a863a3e137fb5dbbdc858
Vasyliev Vadym <protocol222333@gmail.com>
2025-09-05 19:39:59 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const largeImg = document.querySelector('#largeImg');
+  const thumbs = document.querySelector('#thumbs');
+
+  thumbs.addEventListener('click', (ev) => {
+    ev.preventDefault();
+
+    let link;
+    const tName = ev.target.tagName;
+
+    if (tName === 'IMG') {
+      link = ev.target.parentNode;
+    } else if (tName === 'A') {
+      link = ev.target;
+    }
+
+    if (!link) {
+      return;
+    }
+
+    largeImg.src = link.href;
+  });
+});
